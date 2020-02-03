159e5aeae2e30a4a7e3c2e8432eb8965a2085e4f
Tuan Nam Truong <ttnam1995@gmail.com>
2020-02-03 16:33:00 +0100

    Initial commit

---
+++ CarStore/Migrations/20200113072412_Car.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CarStore.Migrations
+{
+    public partial class Car : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Category",
+                table: "Cars",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Category",
+                table: "Cars");
+        }
+    }
+}
---
+++ CarStore/Startup.cs
@@ -0,0 +1,65 @@
+﻿using CarStore.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CarStore
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration) => Configuration = configuration;
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration["Data:CarStoreRepo:ConnectionString"]));
+            services.AddDbContext<AppIdentityDbContext>(options => options.UseSqlServer(Configuration["Data:CarStoreIdentity:ConnectionString"]));
+            services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<AppIdentityDbContext>().AddDefaultTokenProviders();
+            services.AddTransient<ICarRepository, EFCarRepository>();
+            services.AddTransient<IOrderRepository, EFOrderRepository>();
+            services.AddScoped<Cart>(sp => SessionCart.GetCart(sp));                // When an object with datatype Cart is asked, the session will provide an session cart object.
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddMvc();
+            services.AddMemoryCache();
+            services.AddSession();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseAuthentication();
+            app.UseStaticFiles();
+            app.UseStatusCodePages();
+            app.UseSession();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: null,
+                    template: "{currentCategory}/Page{currentPage}",
+                    defaults: new { Controller = "Car", action = "List" });
+                routes.MapRoute(
+                    name: null,
+                    template: "Page{currentPage}",
+                    defaults: new { Controller = "Car", action = "List", currentPage = 1 });
+                routes.MapRoute(
+                    name: null,
+                    template: "{currentCategory}",
+                    defaults: new { Controller = "Car", action = "List", currentPage = 1 });
+                routes.MapRoute(
+                    name: null,
+                    template: "",
+                    defaults: new { Controller = "Car", action = "List", currentPage = 1 });
+                routes.MapRoute(name: null, template: "{controller}/{action}/{id?}");
+            });
+            SeedData.EnsurePopulated(app);
+            IdentitySeedData.EnsurePopulated(app);
+        }
+    }
+}
